9f633ce2e24b0f36d9bf615ca56612a9a50053f2
Pasha2302 <pavelnebrat6021023@gmail.com>
2025-02-18 15:19:50 +0300

    comm_18_02_2025T15:01:00

--- app/controllers_views/page_coin.py
+++ app/controllers_views/page_coin.py
@@ -38,6 +38,16 @@ class CoinPageContextManager:
         contracts_addresses = coin.get_all_contract_addresses()
         base_address = contracts_addresses.filter(basic=True).first()
 
+        # Получение всех описаний из CoinDescription, которые не являются пустыми
+        if hasattr(coin, 'description'):  # Проверяем, есть ли описание у монеты
+            descriptions = [
+                getattr(coin.description, f"desc_{i}")
+                for i in range(6)  # desc_0 - desc_5
+                if getattr(coin.description, f"desc_{i}", None)  # Описание не пустое
+            ]
+        else:
+            descriptions = []
+
         context['obj_coin'] = coin
         context['obj_basic_chain'] = coin.get_chain_from_basic_contract()
         context['contracts_addresses'] = contracts_addresses
@@ -45,6 +55,8 @@ class CoinPageContextManager:
         context['obj_socials'] = coin.socials.all()
 
         context['more_coins'] = Coin.objects.filter(contract_address__chain__slug=self.chain_slug)
+        context['descriptions'] = descriptions  # Добавляем в контекст непустые описания
+
         # print(f"\n\nMore coins: {context['more_coins']}")
         return context
 
